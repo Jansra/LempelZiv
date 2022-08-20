5c00c55e97f4c5faba0f4147b90b57383b70f567
Sravya Jandhyala <sravya.jandhyala@gmail.com>
2022-08-20 06:58:28 +0200

    Improve functionality
    Add longest string match and compression for variable window sizes.

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
 # Lempel-Ziv compression with sliding window
-# Proof of concept for window size of 2
-# This doesn't really lead to an actual compression since
-# 2 characters are replaced by (x,y) but has the opposite effect
 
-# LZ which takes in a string as input and length of window
-def lempelziv(input, length):
-    if len(input) != 0:
-        index = length
-        # window = input[index:index*2]
-        output = input[0:index]
-
-        while (index < len(input)):
-            window = input[index:index+length]
-            input_before = input[0:index-1]
-            if window in input_before:
-                offset = input_before.find(window)
-                output += "(" + str(offset) + "," + str(length) + ")"
+def longest_string_match(input_text, word):
+    match = ""
+    i = 0
+    if input_text[i] == word[i]:
+        match += input_text[i]
+        i += 1
+        while (i < len(input_text)) and (i < len(word)):
+            if input_text[i] == word[i]:
+                match += input_text[i]
+                i += 1
             else:
-                output += window
-            index += length
+                break
+    return len(match)
+
+
+def lempelziv(text, length):
+    if len(text) != 0:
+        index = length
+        output = text[0:index]
+        match = ""
+        while index < len(text):
+            window = text[index:index+length]
+            print("Window: " + '"' + window + '"')
+            input_before = text[0:index-1]
+            print("Input before: " + '"' + input_before + '"')
+            offset = input_before.find(window)
+            print("Offset: " + '"' + str(offset) + '"')
+            if offset == -1:
+                print("----------IF--------------------")
+                output += window[0]
+                print("Output: " + '"' + output + '"')
+                index += 1
+            else:
+                print("----------ELSE--------------------")
+                match += window
+                search_input = text[offset+length:index+1]
+                print("Search input: " + '"' + search_input + '"')
+                search_string = text[index+length:len(text)]
+                print("Search string: " + '"' + search_string + '"')
+                match_length = longest_string_match(search_input, search_string)
+                print("Match length: " + '"' + str(match_length) + '"')
+                output += "("+ str(offset) + "," + str(match_length + length) +")"
+                print("Output: " + '"' + output + '"')
+                index += match_length + length
+                print("--------------------------------------------")
         return output
     else:
-        return ("Not a valid input.")
+        return "Not a valid input."
 
 
 if __name__ == '__main__':
-    input = "This is a string."
-    print(str(lempelziv(input, 2)))
+    text = "This is a string, why it is a string."
+    print(str(lempelziv(text, 2)))
